9d2a95a035aead63ebc78018e5182554546f4608
unknown <avir4m@gmail.com>
2022-01-18 13:40:56 +0200

    Version 1.1

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ folder = "Files"
 renamed_folder = "Renamed_Files"
 
 for count, filename in enumerate(os.listdir(folder)):
-    test, file_extention = os.path.splitext((filename))
+    file_name, file_extention = os.path.splitext((filename))
     new_name = f"{str(count)}{file_extention}"
     src =f"{folder}/{filename}"
     dst =f"{renamed_folder}/{new_name}"
